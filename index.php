c135b67f9bb39af0edfd124dd0c60489f27b8c21
Sharky! <jdeurt@gmail.com>
2018-04-10 14:15:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,4 +12,14 @@ if(!empty($_GET['id'])) {
 
 echo $baseURL . $_SESSION['id'];
 
+$ch     = curl_init($baseURL . $_SESSION['id']);
+$ch_set = array(
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_HEADER => 0,
+    CURLOPT_RETURNTRANSFER => 1
+);
+curl_setopt_array($ch, $ch_set);
+$response = curl_exec($ch);
+echo ($response);
+
 session_destroy();
